599780e220af7109bf9765ea8eb04c7647259148
atharv parlikar <atharvparlikar@gmail.com>
2025-03-24 03:06:44 +0530

    removed dist

--- dist/client.d.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { EventMap } from "./events";
-interface CreateClientArgs {
-    url: string;
-}
-export declare const createZapClient: <TEvents extends EventMap>({ url }: CreateClientArgs) => { [K in keyof TEvents]: {
-    send: TEvents[K]["input"] extends z.ZodVoid ? () => Promise<ReturnType<TEvents[K]["process"]>> : (input: z.infer<TEvents[K]["input"]>) => Promise<ReturnType<TEvents[K]["process"]>>;
-}; };
-export {};
