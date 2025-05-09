31f9052f23aee01d73275ec2c220056848eac47f
atharv parlikar <atharvparlikar@gmail.com>
2025-05-09 17:38:58 +0530

    docs update

--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -1,9 +1,12 @@
-import { createZapClient } from "../../../client/src/index";
+import { createZapClient } from "@zap-socket/client";
 import { Events } from "../server/index";
 
 const url = "ws://localhost:8080/";
 
 function run() {
+  const response = await client.events.message.send("hi"); // returns promise that resolves to server response.
+
+  client.events.message.send("hii"); // fire and forget.
 }
 
 const client = createZapClient<Events>({ url });
